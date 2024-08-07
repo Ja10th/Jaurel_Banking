c613d5d35223dfbc04ea6773ea91bfd9fbbfe1f8
James Oluwaleye <oluwaleyejames@gmail.com>
2024-08-07 17:44:42 +0100

    Working on Form

---
+++ components/CustomInput.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Control, FieldPath } from 'react-hook-form'
+import { z } from 'zod'
+import { formSchema } from '@/lib/utils'
+
+
+
+   
+
+interface CustomInputProps {
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
+    label: string,
+    placeholder: string,
+}
+
+
+const CustomInput = ({ control, name, label, placeholder } : CustomInputProps) => {
+  return (
+    <FormField
+                    control={control}
+                    name={name}
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>{label}</FormLabel>
+                        <FormControl>
+                            <Input placeholder={placeholder} type='password'{...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
+  )
+}
+
+export default CustomInput
